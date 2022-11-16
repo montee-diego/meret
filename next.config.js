1163475ee5a42488119787756d120e07515446bc
Diego Montivero <diego.montivero91@gmail.com>
2022-11-16 20:54:16 +0100

    Project setup

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  distDir: "dist",
   swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
