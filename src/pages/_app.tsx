789bcbed60115e88dbc15f36613569bfba5779ef
Diego Montivero <diego.montivero91@gmail.com>
2022-11-21 21:13:29 +0100

    Created NowPlaying context

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { NowPlayingContext } from "@context/NowPlaying";
+import "../styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <NowPlayingContext>
+      <Component {...pageProps} />
+    </NowPlayingContext>
+  );
 }
