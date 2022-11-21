ec17332e83a0d368617281b58596aae9026f71a1
Diego Montivero <diego.montivero91@gmail.com>
2022-11-21 18:21:57 +0100

    Refactor sanity client

--- src/services/sanity/client.ts
+++ src/services/sanity/client.ts
@@ -1,9 +1,8 @@
 import { default as client } from "@sanity/client";
 
-const sanityClient = client({
-  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+export const sanityClient = client({
+  projectId: process.env.SANITY_PROJECT_ID,
   dataset: "production",
   apiVersion: "2022-11-18",
+  useCdn: true,
 });
-
-export default sanityClient;
--- src/services/sanityImageUrl.ts
+++
@@ -1,7 +0,0 @@
-import imageUrlBuilder from "@sanity/image-url";
-import sanityClient from "@services/sanityClient";
-
-const builder = imageUrlBuilder(sanityClient);
-const sanityImageUrl = (source: string) => builder.image(source);
-
-export default sanityImageUrl;
