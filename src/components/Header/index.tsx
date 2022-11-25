857776954261069afc122f1e545bf9ae09770c86
Diego Montivero <diego.montivero91@gmail.com>
2022-11-25 17:56:07 -0300

    Implemented Sidebar basic component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,17 @@
-import type { FC } from "react";
+import type { FC, Dispatch, SetStateAction } from "react";
 import { useTheme } from "@context/Theme";
 import style from "./index.module.css";
 
-export const Header: FC = () => {
+interface IProps {
+  setIsNavOpen: Dispatch<SetStateAction<boolean>>;
+}
+
+export const Header: FC<IProps> = ({ setIsNavOpen }) => {
   const { setTheme } = useTheme();
 
   return (
     <header className={style.Container}>
-      <h1>Meret</h1>
+      <h1 onClick={() => setIsNavOpen(true)}>*title goes here*</h1>
 
       <button onClick={() => setTheme("light")}>light</button>
       <button onClick={() => setTheme("dark")}>dark</button>
--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,9 +1,24 @@
-import type { FC } from "react";
+import type { FC, Dispatch, SetStateAction } from "react";
+import style from "./index.module.css";
 
-export const Sidebar: FC = () => {
+interface IProps {
+  isNavOpen: boolean;
+  setIsNavOpen: Dispatch<SetStateAction<boolean>>;
+}
+
+export const Sidebar: FC<IProps> = ({ isNavOpen, setIsNavOpen }) => {
+  const handleClose = () => setIsNavOpen(false);
+
   return (
-    <aside>
-      <h1>Sidebar</h1>
-    </aside>
+    <>
+      <nav className={style.Container + (isNavOpen ? " " + style.Open : "")}>
+        <h1 onClick={handleClose}>Meret</h1>
+      </nav>
+
+      <div
+        className={style.Overlay + (isNavOpen ? " " + style.OverlayShow : "")}
+        onClick={handleClose}
+      ></div>
+    </>
   );
 };
