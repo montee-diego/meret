693d38cc0089a7efbe5fed6be81ba63ef4247866
Diego Montivero <diego.montivero91@gmail.com>
2022-11-25 14:15:32 -0300

    Updated Track components with loading state

--- src/components/TrackList/index.tsx
+++ src/components/TrackList/index.tsx
@@ -1,18 +1,19 @@
 import type { FC } from "react";
 import type { ITrack } from "@global/types";
-import { Track } from "@components/index";
+import { Loading, Track } from "@components/index";
 import style from "./index.module.css";
 
 interface IProps {
+  isLoading: boolean;
   tracks: ITrack[];
 }
 
-export const TrackList: FC<IProps> = ({ tracks }) => {
+export const TrackList: FC<IProps> = ({ isLoading, tracks }) => {
+  if (isLoading) return <Loading />;
+
   return (
-    <section className={style.Container}>
-      {tracks.map((track) => (
-        <Track track={track} key={track._id} />
-      ))}
-    </section>
+    <div className={style.Container}>
+      {tracks.map((track) => <Track track={track} key={track._id} />)}
+    </div>
   );
 };
