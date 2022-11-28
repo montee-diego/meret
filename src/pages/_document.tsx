124a42fdbee0676c0d48d21568200ee90248d718
Diego Montivero <diego.montivero91@gmail.com>
2022-11-28 18:27:17 -0300

    Minor fixes

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,8 @@ class PageDocument extends Document {
     return (
       <Html>
         <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <meta name="robots" content="index, follow" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
