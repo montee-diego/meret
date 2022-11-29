78ad4142f7b0995bbcfa3382b22ab4478aaa39e1
Diego Montivero <diego.montivero91@gmail.com>
2022-11-29 20:18:32 -0300

    Moved player state logic to Context

--- src/context/AudioPlayer.tsx
+++ src/context/AudioPlayer.tsx
@@ -7,23 +7,28 @@ interface IProps {
 }
 
 interface IContext {
+  playerOpen: boolean;
   playlist: ITrack[];
+  setPlayerOpen: Dispatch<SetStateAction<boolean>>;
   setPlaylist: Dispatch<SetStateAction<ITrack[]>>;
 }
 
 const AudioPlayer = createContext<IContext>({
+  playerOpen: false,
   playlist: [],
+  setPlayerOpen: () => {},
   setPlaylist: () => {},
 });
 
 export const AudioPlayerContext: FC<IProps> = (props) => {
+  const [playerOpen, setPlayerOpen] = useState<boolean>(false);
   const [playlist, setPlaylist] = useState<ITrack[]>([]);
 
   return (
-    <AudioPlayer.Provider value={{ playlist, setPlaylist }}>
+    <AudioPlayer.Provider value={{ playerOpen, playlist, setPlayerOpen, setPlaylist }}>
       {props.children}
     </AudioPlayer.Provider>
-  )
-}
+  );
+};
 
 export const useAudioPlayer = () => useContext(AudioPlayer);
