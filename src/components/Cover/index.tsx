83664fbd9926bc06913e71a631f02f7590d3cddf
Diego Montivero <diego.montivero91@gmail.com>
2022-11-30 15:30:29 -0300

    Layout update

--- src/components/Cover/index.tsx
+++ src/components/Cover/index.tsx
@@ -1,14 +1,17 @@
 import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEllipsis } from "@fortawesome/free-solid-svg-icons";
 import type { FC, SyntheticEvent } from "react";
-import { CoverPlaceholder } from "@components/index";
+import { CoverPlaceholder, Loading } from "@components/index";
 import style from "./index.module.css";
 
 interface IProps {
   colors: string[];
   cover: string;
+  size: string;
 }
 
-export const Cover: FC<IProps> = ({ colors, cover }) => {
+export const Cover: FC<IProps> = ({ colors, cover, size }) => {
   const [isLoading, setIsLoading] = useState<boolean>(true);
 
   const handleLoad = (event: SyntheticEvent<HTMLImageElement>) => {
@@ -17,9 +20,11 @@ export const Cover: FC<IProps> = ({ colors, cover }) => {
   };
 
   return (
-    <div className={style.Container}>
+    <div className={style.Container} style={{ width: size }}>
       {cover && <img src={cover} alt="No Cover" className={style.Img} onLoad={handleLoad} />}
-      {isLoading && <CoverPlaceholder colors={colors} />}
+      {/* {isLoading && <CoverPlaceholder colors={colors} />} */}
+      {/* {isLoading && <FontAwesomeIcon icon={faEllipsis} bounce size="xl" />} */}
+      {isLoading && <Loading />}
     </div>
   );
 };
