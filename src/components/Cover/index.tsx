4c99c99a77921c413fe333617f5e84349b4d9c08
Diego Montivero <diego.montivero91@gmail.com>
2022-12-01 19:14:22 -0300

    Animation added to Cover

--- src/components/Cover/index.tsx
+++ src/components/Cover/index.tsx
@@ -1,30 +1,26 @@
 import { useState } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faEllipsis } from "@fortawesome/free-solid-svg-icons";
 import type { FC, SyntheticEvent } from "react";
-import { CoverPlaceholder, Loading } from "@components/index";
 import style from "./index.module.css";
 
 interface IProps {
-  colors: string[];
   cover: string;
   size: string;
 }
 
-export const Cover: FC<IProps> = ({ colors, cover, size }) => {
+export const Cover: FC<IProps> = ({ cover, size }) => {
   const [isLoading, setIsLoading] = useState<boolean>(true);
 
+  const loadingClass = isLoading ? " " + style.Loading : "";
   const handleLoad = (event: SyntheticEvent<HTMLImageElement>) => {
     event.currentTarget.classList.add(style.ImgLoaded);
     setIsLoading(false);
   };
 
   return (
-    <div className={style.Container} style={{ width: size }}>
-      {cover && <img src={cover} alt="No Cover" className={style.Img} onLoad={handleLoad} />}
-      {/* {isLoading && <CoverPlaceholder colors={colors} />} */}
-      {/* {isLoading && <FontAwesomeIcon icon={faEllipsis} bounce size="xl" />} */}
-      {isLoading && <Loading />}
+    <div className={style.Container + loadingClass} style={{ width: size }}>
+      {cover && (
+        <img src={cover} className={style.Img} onLoad={handleLoad} alt="No Cover" />
+      )}
     </div>
   );
 };
