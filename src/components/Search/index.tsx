204461bb44753a7d97fa54368c41efbc8177e3de
Diego Montivero <diego.montivero91@gmail.com>
2022-12-02 18:31:16 -0300

    Updated Search logic and style fix

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -1,12 +1,30 @@
+import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
-import type { FC } from "react";
+import type { FC, SyntheticEvent } from "react";
 import style from "./index.module.css";
 
 export const Search: FC = () => {
+  const [input, setInput] = useState<string>("");
+
+  const handleInput = (e: SyntheticEvent<HTMLInputElement>) => {
+    setInput(e.currentTarget.value);
+  };
+
+  const handleSubmit = (e: SyntheticEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("Search:", input);
+  };
+
   return (
-    <form className={style.Container}>
-      <input type="text" className={style.Input} placeholder="Search" />
+    <form className={style.Container} onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className={style.Input}
+        value={input}
+        placeholder="Search"
+        onInput={handleInput}
+      />
       <button type="submit" className={style.Submit}>
         <FontAwesomeIcon icon={faMagnifyingGlass} />
       </button>
