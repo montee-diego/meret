794cd35f8d5894ba0b040d810bbb966a367a80f3
Diego Montivero <diego.montivero91@gmail.com>
2022-12-06 18:50:38 -0300

    Added links and playlist selection to sidebar

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,7 +1,8 @@
-import type { FC, Dispatch, SetStateAction } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faXmark } from "@fortawesome/free-solid-svg-icons";
+import { faChevronDown, faXmark } from "@fortawesome/free-solid-svg-icons";
 import { ButtonIcon, Search } from "@components/index";
+import Link from "next/link";
+import type { FC, Dispatch, SetStateAction } from "react";
 import style from "./index.module.css";
 
 interface IProps {
@@ -23,6 +24,24 @@ export const Sidebar: FC<IProps> = ({ isNavOpen, setIsNavOpen }) => {
         </div>
 
         <Search />
+
+        <div className={style.Links}>
+          <Link href="/">Home</Link>
+          <Link href="/artists">Artists</Link>
+        </div>
+
+        <details className={style.Playlists}>
+          <summary>
+            <FontAwesomeIcon icon={faChevronDown} size="sm" />
+            Playlists
+          </summary>
+          <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+            <li>4</li>
+          </ul>
+        </details>
       </nav>
 
       <div
