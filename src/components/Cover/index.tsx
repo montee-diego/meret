5c1f792681a10c7b4445b8a9aee183647cc2432c
Diego Montivero <diego.montivero91@gmail.com>
2022-12-07 19:50:12 -0300

    Implemented next/image for cover display

--- src/components/Cover/index.tsx
+++ src/components/Cover/index.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
-import type { FC, SyntheticEvent } from "react";
+import Image from "next/image";
+import type { FC } from "react";
 import style from "./index.module.css";
 
 interface IProps {
@@ -11,15 +12,22 @@ export const Cover: FC<IProps> = ({ cover, size }) => {
   const [isLoading, setIsLoading] = useState<boolean>(true);
 
   const loadingClass = isLoading ? " " + style.Loading : "";
-  const handleLoad = (event: SyntheticEvent<HTMLImageElement>) => {
-    event.currentTarget.classList.add(style.ImgLoaded);
+  const handleLoad = (image: HTMLImageElement) => {
+    image.classList.add(style.ImgLoaded);
     setIsLoading(false);
   };
 
   return (
     <div className={style.Container + loadingClass} style={{ width: size }}>
       {cover && (
-        <img src={cover} className={style.Img} onLoad={handleLoad} alt="No Cover" />
+        <Image
+          src={cover}
+          className={style.Img}
+          onLoadingComplete={handleLoad}
+          alt="No Cover"
+          sizes="320px"
+          fill
+        />
       )}
     </div>
   );
