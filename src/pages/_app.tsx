696876a1f93a375893e6a93aa9044b17b8efc761
Diego Montivero <diego.montivero91@gmail.com>
2022-12-12 18:33:43 -0300

    Added AuthProvider to app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { SessionProvider } from "next-auth/react";
 import { ThemeContext } from "@context/Theme";
 import { AudioPlayerContext } from "@context/AudioPlayer";
 import { AudioPlayer, Header, Main, Sidebar } from "@components/index";
@@ -6,26 +7,26 @@ import Head from "next/head";
 import type { AppProps } from "next/app";
 import "@styles/globals.css";
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   const [isNavOpen, setIsNavOpen] = useState<boolean>(false);
 
   return (
-    <ThemeContext>
-      <AudioPlayerContext>
-        <Head>
-          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        </Head>
+    <SessionProvider session={session}>
+      <ThemeContext>
+        <AudioPlayerContext>
+          <Head>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          </Head>
 
-        <Sidebar isNavOpen={isNavOpen} setIsNavOpen={setIsNavOpen} />
-        <Header setIsNavOpen={setIsNavOpen} />
+          <Sidebar isNavOpen={isNavOpen} setIsNavOpen={setIsNavOpen} />
+          <Header setIsNavOpen={setIsNavOpen} />
 
-        {/* <div style={{ display: "flex" }}> */}
-        <Main>
-          <Component {...pageProps} />
-          <AudioPlayer />
-        </Main>
-        {/* </div> */}
-      </AudioPlayerContext>
-    </ThemeContext>
+          <Main>
+            <Component {...pageProps} />
+            <AudioPlayer />
+          </Main>
+        </AudioPlayerContext>
+      </ThemeContext>
+    </SessionProvider>
   );
 }
