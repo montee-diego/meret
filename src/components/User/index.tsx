a306899bc88e5aa2935d8beb850c8c668c37d24c
Diego Montivero <diego.montivero91@gmail.com>
2022-12-13 14:59:15 -0300

    Updated User component

--- src/components/User/index.tsx
+++ src/components/User/index.tsx
@@ -1,20 +1,42 @@
+import { useState } from "react";
 import { useSession, signIn, signOut } from "next-auth/react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleUser } from "@fortawesome/free-solid-svg-icons";
+import { ButtonIcon } from "@components/index";
 import type { FC } from "react";
+import style from "./index.module.css";
 
 export const User: FC = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
   const { data: session } = useSession();
+  const handleUserMenu = () => setIsMenuOpen(!isMenuOpen);
+  const handleLogIn = () => signIn("google");
+  const handleLogOut = () => signOut();
 
   return (
-    <div>
-      {session ? (
-        <>
-          <img src={`${session.user?.image}`} alt="" width={24} />
-          <p>{session.user?.name}</p>
-          <button onClick={() => signOut()}>logout</button>
-        </>
-      ) : (
-        <button onClick={() => signIn("google")}>login</button>
-      )}
+    <div className={style.MenuButton}>
+      <ButtonIcon onClick={handleUserMenu}>
+        {session ? (
+          <img src={`${session.user?.image}`} alt="" />
+        ) : (
+          <FontAwesomeIcon size="xl" icon={faCircleUser} />
+        )}
+      </ButtonIcon>
+
+      <div className={style.Menu + (isMenuOpen ? " " + style.Open : "")}>
+        {session ? (
+          <div>
+            <p>{session.user?.name}</p>
+            <button className={style.Button} onClick={handleLogOut}>
+              Log Out
+            </button>
+          </div>
+        ) : (
+          <button className={style.Button} onClick={handleLogIn}>
+            Log In
+          </button>
+        )}
+      </div>
     </div>
   );
 };
