6a1348bbcb05a7d88be1dce559c5d0fe60c23533
Diego Montivero <diego.montivero91@gmail.com>
2022-12-26 15:50:36 -0300

    Implemented forwardRef

--- src/components/ButtonIcon/index.tsx
+++ src/components/ButtonIcon/index.tsx
@@ -1,15 +1,20 @@
+import { forwardRef } from "react";
+
 import type { FC, ReactNode } from "react";
 import style from "./index.module.css";
 
 interface IProps {
   children: ReactNode;
+  label: string;
   onClick: () => void;
 }
 
-export const ButtonIcon: FC<IProps> = ({ children, onClick }) => {
+export const ButtonIcon = forwardRef<HTMLButtonElement, IProps>(function ButtonIcon(props, ref) {
+  const { children, label, onClick } = props;
+
   return (
-    <button className={style.Button} onClick={onClick}>
+    <button className={style.Button} onClick={onClick} aria-label={label} ref={ref}>
       {children}
     </button>
   );
-};
+});
