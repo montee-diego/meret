cfd32be2f6d3731f3660f6582b89e698c06aa429
Diego Montivero <diego.montivero91@gmail.com>
2022-12-28 16:54:54 -0300

    Added Modal and MenuTrack

--- src/components/TrackList/index.tsx
+++ src/components/TrackList/index.tsx
@@ -1,19 +1,29 @@
-import { Loading, Track } from "@components/index";
+import { useModal } from "src/hooks/useModal";
+import { Modal, Track, TrackMenu } from "@components/index";
 import type { FC } from "react";
 import type { ITrack } from "@global/types";
 import style from "./index.module.css";
 
 interface IProps {
-  isLoading: boolean;
   tracks: ITrack[];
 }
 
-export const TrackList: FC<IProps> = ({ isLoading, tracks }) => {
-  if (isLoading) return <Loading />;
+export const TrackList: FC<IProps> = ({ tracks }) => {
+  const { content, setContent } = useModal();
 
   return (
-    <div className={style.Container}>
-      {tracks.map((track) => <Track track={track} key={track._id} />)}
-    </div>
+    <>
+      <div className={style.Container}>
+        {tracks.map((track) => (
+          <Track track={track} setContent={setContent} key={track._id} />
+        ))}
+      </div>
+
+      {content && (
+        <Modal setContent={setContent}>
+          <TrackMenu content={content} setContent={setContent} />
+        </Modal>
+      )}
+    </>
   );
 };
