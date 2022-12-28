de5fc608abca4a69b855338f8c4a41d9ccc5422e
Diego Montivero <diego.montivero91@gmail.com>
2022-12-28 16:53:44 -0300

    Updated component to use FocusTrap

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,11 +1,11 @@
-import { useEffect, useRef } from "react";
 import { useRouter } from "next/router";
 import { useSession } from "next-auth/react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChevronDown, faXmark } from "@fortawesome/free-solid-svg-icons";
+import { FocusTrap } from "@accessibility/FocusTrap";
 import { ButtonIcon, Search } from "@components/index";
 import Link from "next/link";
-import type { FC, Dispatch, SetStateAction, FocusEvent, KeyboardEvent } from "react";
+import type { FC, Dispatch, SetStateAction } from "react";
 import style from "./index.module.css";
 
 interface IProps {
@@ -16,45 +16,23 @@ interface IProps {
 export const Sidebar: FC<IProps> = ({ isNavOpen, setIsNavOpen }) => {
   const { pathname } = useRouter();
   const { data: session } = useSession();
-  const closeRef = useRef<HTMLButtonElement | null>(null);
 
   const handleNavClose = () => setIsNavOpen(false);
-
-  const handleKey = (event: KeyboardEvent) => {
-    if (event.code === "Escape" && isNavOpen) {
-      setIsNavOpen(false);
-    }
-  };
-
-  const handleFocus = ({ currentTarget }: FocusEvent<HTMLElement>) => {
-    if (!currentTarget.matches(":focus-within")) {
-      setIsNavOpen(false);
-    }
-  };
 
   const setActiveClass = (href: string): string => {
     return pathname === href ? style.Active : "";
   };
 
-  useEffect(() => {
-    if (isNavOpen) {
-      if (closeRef.current) closeRef.current.focus();
-    } else {
-      if (document.activeElement instanceof HTMLElement) document.activeElement.blur();
-    }
-  }, [isNavOpen]);
-
   return (
-    <div className={style.Container + (isNavOpen ? " " + style.View : "")}>
-      <nav
+    <div className={style.Container + (isNavOpen ? " " + style.View : "")} onClick={handleNavClose}>
+      <FocusTrap
+        active={isNavOpen}
         className={style.Menu + (isNavOpen ? " " + style.Open : "")}
-        onKeyDown={handleKey}
-        onBlur={handleFocus}
-        tabIndex={-1}
+        cancelEvent={handleNavClose}
       >
         <div className={style.Logo}>
           <h1>Meret</h1>
-          <ButtonIcon onClick={handleNavClose} label={"close sidebar"} ref={closeRef}>
+          <ButtonIcon onClick={handleNavClose} label={"close sidebar"}>
             <FontAwesomeIcon icon={faXmark} size="xl" />
           </ButtonIcon>
         </div>
@@ -90,7 +68,7 @@ export const Sidebar: FC<IProps> = ({ isNavOpen, setIsNavOpen }) => {
             )}
           </div>
         </details>
-      </nav>
+      </FocusTrap>
     </div>
   );
 };
