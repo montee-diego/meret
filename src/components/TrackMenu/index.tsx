af88cfc5e1401a384b4c4120be9cd310c208e421
Diego Montivero <diego.montivero91@gmail.com>
2023-01-02 13:19:32 -0300

    Updated menu display info and style

--- src/components/TrackMenu/index.tsx
+++ src/components/TrackMenu/index.tsx
@@ -1,8 +1,9 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faXmark } from "@fortawesome/free-solid-svg-icons";
-import { ButtonIcon } from "@components/ButtonIcon";
+import { ButtonIcon, Cover } from "@components/index";
 import type { Dispatch, FC, SetStateAction } from "react";
 import type { ITrack } from "@global/types";
+import style from "./index.module.css";
 
 interface IProps {
   content: ITrack | null;
@@ -13,13 +14,23 @@ export const TrackMenu: FC<IProps> = ({ content, setContent }) => {
   const handleClose = () => setContent(null);
 
   return (
-    <div>
-      <p>trackMenu</p>
-      <ButtonIcon onClick={handleClose} label={"close modal"}>
-        <FontAwesomeIcon icon={faXmark} size={"xl"} />
-      </ButtonIcon>
-      <a href="#">link</a>
-      <a href="#">link</a>
+    <div className={style.Container}>
+      <div className={style.Track}>
+        <Cover cover={`${content?.cover}`} size={"50px"} />
+
+        <div className={style.Data}>
+          <p>{content?.title}</p>
+          <p>{content?.artist}</p>
+        </div>
+
+        <ButtonIcon onClick={handleClose} label={"close modal"}>
+          <FontAwesomeIcon icon={faXmark} size={"xl"} />
+        </ButtonIcon>
+      </div>
+      <p>Action 1</p>
+
+      <a href="#">Action 2</a>
+      <a href="#">Action 3</a>
     </div>
   );
 };
