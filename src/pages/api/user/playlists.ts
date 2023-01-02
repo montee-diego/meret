478dd38a211a82552c39830176f624d42cdc108d
Diego Montivero <diego.montivero91@gmail.com>
2023-01-02 17:11:37 -0300

    Added api route to fetch playlists

---
+++ src/pages/api/user/playlists.ts
@@ -0,0 +1,23 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { getToken } from "next-auth/jwt";
+import { sanityClient } from "@services/sanity/client";
+import { queryUserPlaylists } from "@services/sanity/queries";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === "GET") {
+    const token = await getToken({ req });
+
+    if (token) {
+      // @ts-ignore
+      const response = await sanityClient.fetch(queryUserPlaylists(token.id));
+
+      if (response) {
+        res.status(200).json(response);
+      } else {
+        res.status(500).json({ error: "Failed to fetch user playlists" });
+      }
+    } else {
+      res.status(401);
+    }
+  }
+}
