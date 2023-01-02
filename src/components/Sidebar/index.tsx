a4bbe6bff8e262475a0bac3c8129f02782925aff
Diego Montivero <diego.montivero91@gmail.com>
2023-01-02 17:10:03 -0300

    Component now loads user playlists on login

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,9 +1,11 @@
+import { useEffect } from "react";
 import { useRouter } from "next/router";
-import { useSession } from "next-auth/react";
+import { useSession, signIn } from "next-auth/react";
+import { useUser } from "@context/User";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChevronDown, faXmark } from "@fortawesome/free-solid-svg-icons";
 import { FocusTrap } from "@accessibility/FocusTrap";
-import { ButtonIcon, Search } from "@components/index";
+import { ButtonIcon, Search, UserPlaylists } from "@components/index";
 import Link from "next/link";
 import type { FC, Dispatch, SetStateAction } from "react";
 import style from "./index.module.css";
@@ -15,13 +17,29 @@ interface IProps {
 
 export const Sidebar: FC<IProps> = ({ isNavOpen, setIsNavOpen }) => {
   const { pathname } = useRouter();
-  const { data: session } = useSession();
+  const { data: session, status } = useSession();
+  const { setPlaylists } = useUser();
 
   const handleNavClose = () => setIsNavOpen(false);
+  const handleLogIn = () => signIn("google");
 
   const setActiveClass = (href: string): string => {
     return pathname === href ? style.Active : "";
   };
+
+  const fetchPlaylists = async () => {
+    const response = await fetch("/api/user/playlists");
+    const data = await response.json();
+
+    setPlaylists(data);
+    console.log(data);
+  };
+
+  useEffect(() => {
+    if (session) {
+      fetchPlaylists();
+    }
+  }, []);
 
   return (
     <div className={style.Container + (isNavOpen ? " " + style.View : "")}>
@@ -54,15 +72,10 @@ export const Sidebar: FC<IProps> = ({ isNavOpen, setIsNavOpen }) => {
             <span>Playlists</span>
           </summary>
           <div className={style.PlaylistsContent}>
-            {session ? (
-              <ul>
-                <li>1</li>
-                <li>2</li>
-                <li>3</li>
-                <li>4</li>
-              </ul>
+            {status === "authenticated" ? (
+              <UserPlaylists />
             ) : (
-              <button>
+              <button onClick={handleLogIn}>
                 <strong>Log In</strong> to view playlists
               </button>
             )}
