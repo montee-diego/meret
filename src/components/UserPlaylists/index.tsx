96fc58f297e3dc2a9ba299d6ed9e6524570e00d1
Diego Montivero <diego.montivero91@gmail.com>
2023-01-03 19:49:23 -0300

    Implemented playlist creation and Toast

--- src/components/UserPlaylists/index.tsx
+++ src/components/UserPlaylists/index.tsx
@@ -1,28 +1,66 @@
+import { useRef, useState } from "react";
+import { toast } from "react-hot-toast";
 import { useUser } from "@context/User";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPlus } from "@fortawesome/free-solid-svg-icons";
-import type { FC, FormEvent } from "react";
+import { faPlus, faSpinner } from "@fortawesome/free-solid-svg-icons";
+import Link from "next/link";
+import type { FC, SyntheticEvent } from "react";
 import style from "./index.module.css";
 
 export const UserPlaylists: FC = () => {
-  const { playlists } = useUser();
+  const { playlists, setPlaylists } = useUser();
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const input = useRef<HTMLInputElement | null>(null);
 
-  const handleSubmit = (event: FormEvent) => {
+  const handleSubmit = async (event: SyntheticEvent<HTMLFormElement>) => {
     event.preventDefault();
+
+    if (input.current?.value === "" || isLoading) {
+      return;
+    }
+
+    setIsLoading(true);
+
+    const request = fetch("/api/playlist/create", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ name: input.current?.value }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setIsLoading(false);
+        setPlaylists((prevPlaylists) => [...prevPlaylists, data]);
+
+        if (input.current) {
+          input.current.value = "";
+        }
+      });
+
+    toast.promise(request, {
+      loading: "Creating playlist...",
+      success: "Playlist created!",
+      error: "Failed to create playlist",
+    });
   };
 
   return (
     <>
       <form className={style.Form} onSubmit={handleSubmit}>
-        <input type="text" size={2} placeholder="Create Playlist" required />
+        <input type="text" placeholder="Create Playlist" size={2} ref={input} />
         <button type="submit" tabIndex={-1} aria-label="create playlist">
-          <FontAwesomeIcon icon={faPlus} />
+          {isLoading ? (
+            <FontAwesomeIcon icon={faSpinner} spin />
+          ) : (
+            <FontAwesomeIcon icon={faPlus} />
+          )}
         </button>
       </form>
       <ul className={style.List}>
         {playlists.map((playlist) => (
-          <li>
-            <a href={`/playlist/${playlist.id}`}>{playlist.name}</a>
+          <li key={playlist._id}>
+            <Link href={`/playlist/${playlist._id}`}>{playlist.name}</Link>
           </li>
         ))}
       </ul>
