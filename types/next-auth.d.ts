f376b1223fcfdca07d96d6ca6fec9b61b26540fe
Diego Montivero <diego.montivero91@gmail.com>
2023-01-03 19:30:50 -0300

    Augmented session and JWT types in next-auth

---
+++ types/next-auth.d.ts
@@ -0,0 +1,16 @@
+import NextAuth, { DefaultSession } from "next-auth";
+import { DefaultJWT } from "next-auth/jwt";
+
+declare module "next-auth" {
+  interface Session {
+    user: {
+      id: string;
+    } & DefaultSession["user"];
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    id: string & DefaultJWT;
+  }
+}
