7364c5fb1fc046e1682eeaca9ffc89489f2a0f9d
Diego Montivero <diego.montivero91@gmail.com>
2023-01-06 15:57:11 -0300

    Updated delete playlist logic

--- src/pages/playlist/[id].tsx
+++ src/pages/playlist/[id].tsx
@@ -1,19 +1,47 @@
-import { sanityClient } from "@services/sanity/client";
-import { queryPlaylist } from "@services/sanity/queries";
 import type { GetServerSideProps } from "next";
 import type { IPlaylist } from "@global/types";
+
+import { useRouter } from "next/router";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
+import { useUser } from "@context/User";
+import { useModal } from "@hooks/useModal";
+import { sanityClient } from "@services/sanity/client";
+import { queryPlaylist } from "@services/sanity/queries";
+import { ButtonIcon, ConfirmDialog, Modal } from "@components/index";
 
 interface IProps {
   playlist: IPlaylist;
 }
 
 export default function Playlist({ playlist }: IProps) {
-  console.log(playlist);
-  // const del1 = await sanityClient.delete("");
+  const router = useRouter();
+  const { playlists } = useUser();
+  const { isOpen, toggleOpen } = useModal();
+
+  const handleDelete = async () => {
+    const status = await playlists.delete(playlist._id);
+
+    if (status === "success") {
+      router.replace("/");
+    }
+  };
 
   return (
     <section>
       <p>{playlist.name}</p>
+      <ButtonIcon onClick={toggleOpen} label="delete playlist">
+        <FontAwesomeIcon icon={faTrash} size="xl" />
+      </ButtonIcon>
+
+      {isOpen && (
+        <Modal toggleOpen={toggleOpen}>
+          <ConfirmDialog onConfirm={handleDelete} onCancel={toggleOpen}>
+            Are you sure you want to delete playlist "{playlist.name}"? This action cannot be
+            undone.
+          </ConfirmDialog>
+        </Modal>
+      )}
     </section>
   );
 }
