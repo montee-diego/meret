0d738cbc3a330e8b770d0f0731d690cbb1c4bbbe
Diego Montivero <diego.montivero91@gmail.com>
2023-01-11 17:26:52 -0300

    Updated context

--- src/context/User.tsx
+++ src/context/User.tsx
@@ -1,9 +1,12 @@
 import type { FC, ReactNode } from "react";
 import type { IPlaylist } from "@global/types";
 
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
+import { useSession } from "next-auth/react";
 import { toast } from "react-hot-toast";
-import { fetchPls, createPls, deletePls } from "@services/meret/playlists";
+import { usePlaylists } from "@reducers/usePlaylists";
+import { fetchPls, createPls, deletePls, renamePls } from "@services/meret/playlists";
+import { addItemTo } from "@services/meret/playlistsItems";
 
 interface IProps {
   children: ReactNode;
@@ -12,9 +15,12 @@ interface IProps {
 interface IContext {
   playlists: {
     data: IPlaylist[];
+    fetchError: boolean;
     fetch: () => Promise<any>;
     create: (name: string) => Promise<any>;
     delete: (id: string) => Promise<any>;
+    rename: (id: string, name: string) => Promise<any>;
+    addItem: (playlist: string, track: string) => Promise<any>;
   };
 }
 
@@ -23,38 +29,37 @@ const DummyPromise = new Promise((resolve, reject) => {});
 const User = createContext<IContext>({
   playlists: {
     data: [],
+    fetchError: false,
     fetch: () => DummyPromise,
     create: () => DummyPromise,
     delete: () => DummyPromise,
+    rename: () => DummyPromise,
+    addItem: () => DummyPromise,
   },
 });
 
 export const UserContext: FC<IProps> = (props) => {
-  const [data, setData] = useState<IPlaylist[]>([]);
-
-  function onFetch(data: IPlaylist[]): void {
-    setData(data);
-  }
-
-  const onCreate = (playlist: IPlaylist): void => {
-    setData((prevState) => [...prevState, playlist]);
-  };
-
-  const onDelete = (id: string): void => {
-    setData((prevState) => prevState.filter((pls) => pls._id !== id));
-  };
+  const { status } = useSession();
+  const [data, dispatch] = usePlaylists();
+  const [isFetchError, setIsFetchError] = useState<boolean>(false);
 
   const playlists = {
     data: data,
+    fetchError: isFetchError,
     fetch: async () => {
-      const request = fetchPls(onFetch);
+      const request = fetchPls(dispatch);
+      const status = await request.then((done) => "success").catch((error) => "error");
 
-      return await request.then((done) => "success").catch((error) => "error");
+      if (status === "success") {
+        setIsFetchError(false);
+      } else {
+        setIsFetchError(true);
+      }
+
+      //return await request.then((done) => "success").catch((error) => "error");
     },
     create: async (name: string) => {
-      const request = createPls(name, onCreate);
-
-      toast.promise(request, {
+      const request = toast.promise(createPls(name, dispatch), {
         loading: "Creating playlist...",
         success: "Playlist created!",
         error: "Failed to create playlist",
@@ -63,9 +68,7 @@ export const UserContext: FC<IProps> = (props) => {
       return await request.then((done) => "success").catch((error) => "error");
     },
     delete: async (id: string) => {
-      const request = deletePls(id, onDelete);
-
-      toast.promise(request, {
+      const request = toast.promise(deletePls(id, dispatch), {
         loading: "Deleting playlist...",
         success: "Playlist deleted!",
         error: "Failed to delete playlist",
@@ -73,7 +76,31 @@ export const UserContext: FC<IProps> = (props) => {
 
       return await request.then((done) => "success").catch((error) => "error");
     },
+    rename: async (id: string, name: string) => {
+      const request = toast.promise(renamePls(id, name, dispatch), {
+        loading: "Renaming playlist...",
+        success: "Playlist renamed!",
+        error: "Failed to rename playlist",
+      });
+
+      return await request.then((done) => "success").catch((error) => "error");
+    },
+    addItem: async (playlist: string, track: string) => {
+      const request = toast.promise(addItemTo(playlist, track), {
+        loading: "Adding to playlist...",
+        success: "Added to playlist!",
+        error: "Failed to add to playlist",
+      });
+
+      return await request.then((done) => "success").catch((error) => "error");
+    },
   };
+
+  useEffect(() => {
+    if (status === "authenticated") {
+      playlists.fetch();
+    }
+  }, [status]);
 
   return <User.Provider value={{ playlists }}>{props.children}</User.Provider>;
 };
