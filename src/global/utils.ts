5f4c585825a127760647a7903f3f3e7c4e8d94da
Diego Montivero <diego.montivero91@gmail.com>
2023-01-20 15:51:17 -0300

    Added formatDate function

--- src/global/utils.ts
+++ src/global/utils.ts
@@ -1,3 +1,7 @@
-export const formatTime = (time: number): string => {
+export function formatTime(time: number): string {
   return new Date(time * 1000).toISOString().slice(15, 19);
-};
+}
+
+export function formatDate(date: string): string {
+  return new Intl.DateTimeFormat().format(new Date(date));
+}
