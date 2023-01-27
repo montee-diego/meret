b2c3331cacc86ba6ba0d4a59e86d69cb424d4309
Diego Montivero <diego.montivero91@gmail.com>
2023-01-27 14:18:28 -0300

    Replaced context logic, component now controls theme

--- src/components/ThemeToggle/index.tsx
+++ src/components/ThemeToggle/index.tsx
@@ -1,15 +1,19 @@
 import type { FC } from "react";
 
+import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSun, faMoon } from "@fortawesome/free-solid-svg-icons";
-import { useTheme } from "@context/Theme";
 import { ButtonIcon } from "@components/index";
 
-export const ThemeToggle: FC = () => {
-  const { theme, setTheme } = useTheme();
+const ThemeToggle: FC = () => {
+  const [theme, setTheme] = useState<string>(`${document.body.dataset.theme}`);
 
   const handleThemeToggle = () => {
     const activeTheme = theme === "light" ? "dark" : "light";
+
+    if (theme == activeTheme) {
+      return;
+    }
 
     document.body.dataset.theme = activeTheme;
     window.localStorage.setItem("theme", activeTheme);
@@ -22,3 +26,5 @@ export const ThemeToggle: FC = () => {
     </ButtonIcon>
   );
 };
+
+export default ThemeToggle;
