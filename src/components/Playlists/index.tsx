9d78f3976f497d732560edd188adb6fa619265ae
Diego Montivero <diego.montivero91@gmail.com>
2023-01-31 17:15:42 -0300

    New component to display playlists in a list

---
+++ src/components/Playlists/index.tsx
@@ -0,0 +1,30 @@
+import type { FC } from "react";
+import type { IPlaylistMin } from "@global/types";
+
+import { ButtonText } from "@components/index";
+import style from "./index.module.css";
+
+interface IProps {
+  onClick?: (id: string) => Promise<unknown>;
+  playlists: IPlaylistMin[];
+}
+
+export const Playlists: FC<IProps> = ({ onClick, playlists }) => {
+  return (
+    <ul className={style.List}>
+      {playlists.map((playlist) => (
+        <li key={playlist._id}>
+          {onClick ? (
+            <ButtonText onClick={() => onClick(playlist._id)} align="left">
+              {playlist.name}
+            </ButtonText>
+          ) : (
+            <ButtonText href={`/playlist/${playlist._id}`} align="left">
+              {playlist.name}
+            </ButtonText>
+          )}
+        </li>
+      ))}
+    </ul>
+  );
+};
