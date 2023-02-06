e34df4b0a88ecfbf2d5690af27573f1690714e6f
Diego Montivero <diego.montivero91@gmail.com>
2023-02-06 20:44:42 -0300

    Updated component with playlist delete logic

--- src/components/TrackMenu/index.tsx
+++ src/components/TrackMenu/index.tsx
@@ -1,9 +1,12 @@
 import type { FC } from "react";
 import type { ITrack } from "@global/types";
 
+import { useState } from "react";
+import { useRouter } from "next/router";
 import { signIn, useSession } from "next-auth/react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faXmark } from "@fortawesome/free-solid-svg-icons";
+import { useUser } from "@context/User";
 import { ButtonIcon, ButtonText, Cover, UserPlaylists } from "@components/index";
 import style from "./index.module.css";
 
@@ -14,10 +17,27 @@ interface IProps {
 }
 
 export const TrackMenu: FC<IProps> = ({ isAuthor, track, toggleOpen }) => {
+  const [isConfirm, setIsConfirm] = useState<boolean>(false);
   const { status } = useSession();
+  const { playlists } = useUser();
+  const router = useRouter();
 
+  const toggleConfirm = () => setIsConfirm(!isConfirm);
   const handleLogIn = () => signIn("google");
   const handleAddToPlaylist = () => track;
+
+  async function handleDelete() {
+    const pid = router.query.id as string;
+    const key = `${track?._key}`;
+    const response = await playlists.removeItem(pid, key);
+
+    if (response === "success") {
+      console.log("Removed OK");
+      router.replace(router.asPath, "", {
+        scroll: false,
+      });
+    }
+  }
 
   return (
     <div className={style.Container}>
@@ -34,9 +54,24 @@ export const TrackMenu: FC<IProps> = ({ isAuthor, track, toggleOpen }) => {
         </ButtonIcon>
       </div>
 
-      <div className={style.Playlists} tabIndex={-1}>
+      {isAuthor && (
+        <div className={style.Delete}>
+          <ButtonText onClick={() => setIsConfirm(true)} align="left">
+            Delete from Playlist
+          </ButtonText>
+        </div>
+      )}
+
+      <div className={style.Playlists} tabIndex={-1} data-collapse={isConfirm}>
         {status === "authenticated" ? (
-          <UserPlaylists onAdd={handleAddToPlaylist} />
+          isConfirm ? (
+            <p className={style.DeleteMsg}>
+              Are you sure you want to delete the currently selected track? This action cannot be
+              undone.
+            </p>
+          ) : (
+            <UserPlaylists onAdd={handleAddToPlaylist} />
+          )
         ) : (
           <div className={style.LogIn}>
             <div className={style.LogInBtn}>
@@ -49,16 +84,24 @@ export const TrackMenu: FC<IProps> = ({ isAuthor, track, toggleOpen }) => {
       </div>
 
       <div className={style.QuickActions}>
-        <ButtonText onClick={() => {}} align="center">
-          Play
-        </ButtonText>
-        <ButtonText onClick={() => {}} align="center">
-          Queue
-        </ButtonText>
-        {isAuthor && (
-          <ButtonText onClick={() => {}} align="center">
-            Delete
-          </ButtonText>
+        {isConfirm ? (
+          <>
+            <ButtonText onClick={handleDelete} align="center">
+              Confirm
+            </ButtonText>
+            <ButtonText onClick={toggleConfirm} align="center">
+              Cancel
+            </ButtonText>
+          </>
+        ) : (
+          <>
+            <ButtonText onClick={() => {}} align="center">
+              Play
+            </ButtonText>
+            <ButtonText onClick={() => {}} align="center">
+              Queue
+            </ButtonText>
+          </>
         )}
       </div>
     </div>
