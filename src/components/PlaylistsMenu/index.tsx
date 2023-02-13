c6cf8a0e033e29aeea47736550cad8ffd0b37bc1
Diego Montivero <diego.montivero91@gmail.com>
2023-02-13 16:57:39 -0300

    Implemented new Input component

--- src/components/PlaylistsMenu/index.tsx
+++ src/components/PlaylistsMenu/index.tsx
@@ -1,12 +1,13 @@
-import type { FC, SyntheticEvent } from "react";
+import type { FC } from "react";
 import type { IPlaylistMin } from "@global/types";
 
-import { useRef, useState } from "react";
+import { useState } from "react";
 import { useMeret } from "@context/Meret";
 import { useAudioPlayer } from "@context/AudioPlayer";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlus, faSpinner } from "@fortawesome/free-solid-svg-icons";
 import { ButtonLink } from "@components/index";
+import Input from "@components/Input";
 import style from "./index.module.css";
 
 interface IProps {
@@ -19,23 +20,12 @@ export const PlaylistsMenu: FC<IProps> = ({ getTrackId, showInput, playlists }) 
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const { meret } = useMeret();
   const { player } = useAudioPlayer();
-  const input = useRef<HTMLInputElement | null>(null);
 
-  async function handleCreate(event: SyntheticEvent<HTMLFormElement>) {
-    event.preventDefault();
-    const name = input.current?.value;
-
-    if (isLoading || !name || name === "") {
-      return;
-    }
+  async function handleCreate(input: string) {
+    if (isLoading) return;
 
     setIsLoading(true);
-    const response = await meret.create(name);
-
-    if (response === "OK" && input.current) {
-      input.current.value = "";
-    }
-
+    const response = await meret.create(input);
     setIsLoading(false);
   }
 
@@ -54,16 +44,13 @@ export const PlaylistsMenu: FC<IProps> = ({ getTrackId, showInput, playlists }) 
   return (
     <>
       {showInput && (
-        <form className={style.Form} onSubmit={handleCreate}>
-          <input type="text" placeholder="Create Playlist" size={2} ref={input} />
-          <button type="submit" tabIndex={-1} aria-label="create playlist">
-            {isLoading ? (
-              <FontAwesomeIcon icon={faSpinner} size="lg" spin />
-            ) : (
-              <FontAwesomeIcon icon={faPlus} size="lg" />
-            )}
-          </button>
-        </form>
+        <Input onSubmit={handleCreate} placeholder="Create Playlist">
+          {isLoading ? (
+            <FontAwesomeIcon icon={faSpinner} size="lg" spin />
+          ) : (
+            <FontAwesomeIcon icon={faPlus} size="lg" />
+          )}
+        </Input>
       )}
 
       <ul className={style.List}>
