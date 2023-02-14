a0b2de09d8e4b6eb22c23dd8dbaa1c1d9f50af60
Diego Montivero <diego.montivero91@gmail.com>
2023-02-14 19:18:36 -0300

    Code style update

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,21 +1,21 @@
-import type { FC, ReactNode } from "react";
+import type { ReactNode } from "react";
 
 import { createPortal } from "react-dom";
 import { FocusTrap } from "@accessibility/FocusTrap";
-import style from "./index.module.css";
+import css from "./index.module.css";
 
 interface IProps {
   children: ReactNode;
   toggleOpen: () => void;
 }
 
-export const Modal: FC<IProps> = ({ children, toggleOpen }) => {
+export default function Modal({ children, toggleOpen }: IProps) {
   return createPortal(
-    <div className={style.Container}>
-      <FocusTrap active={true} className={style.Content} cancelEvent={toggleOpen}>
+    <div className={css.Container}>
+      <FocusTrap active className={css.Content} cancelEvent={toggleOpen}>
         {children}
       </FocusTrap>
     </div>,
     document.body
   );
-};
+}
