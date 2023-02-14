4275c9db993f2877ffb3bc761ea1faba95af9d3b
Diego Montivero <diego.montivero91@gmail.com>
2023-02-13 21:48:41 -0300

    Updated component index

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,3 @@
-export { Accordion } from "./Accordion";
 export { AudioControls } from "./AudioControls";
 export { AudioPlayer } from "./AudioPlayer";
 export { Button } from "./Button";
@@ -8,12 +7,9 @@ export { Cover } from "./Cover";
 export { Header } from "./Header";
 export { List } from "./List";
 export { Loading } from "./Loading";
-export { Main } from "./Main";
 export { Menu } from "./Menu";
 export { Modal } from "./Modal";
 export { Playlists } from "./Playlists";
-export { PlaylistsMenu } from "./PlaylistsMenu";
-export { Sidebar } from "./Sidebar";
 export { Tracks } from "./Tracks";
 export { TracksMenu } from "./TracksMenu";
 export { User } from "./User";
