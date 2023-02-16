ee0e93938836eb6fce7e04472ee500b21487c57f
Diego Montivero <diego.montivero91@gmail.com>
2023-02-16 19:45:52 -0300

    Fixed flicker by using useCallback on render function

--- src/hooks/useModal.tsx
+++ src/hooks/useModal.tsx
@@ -1,10 +1,26 @@
-import { useState } from "react";
+import type { ReactElement, ReactNode } from "react";
+import { useCallback, useState } from "react";
 
-type ModalTuple = [boolean, () => void];
+import Modal from "@components/Modal";
+
+interface IProps {
+  children: ReactNode;
+}
+
+type ModalTuple = [() => void, (props: IProps) => ReactElement | null];
 
 export const useModal = (): ModalTuple => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
   const toggleOpen = () => setIsOpen(!isOpen);
 
-  return [isOpen, toggleOpen];
+  // useCallback prevents re-render flicker when the AudioPlayer context updates
+  const RenderModal = useCallback(function RenderModal(props: IProps) {
+    if (isOpen) {
+      return <Modal toggleOpen={toggleOpen}>{props.children}</Modal>;
+    } else {
+      return null;
+    }
+  }, [isOpen]);
+
+  return [toggleOpen, RenderModal];
 };
