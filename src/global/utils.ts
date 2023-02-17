23b0e46407bcd53715a24ac09ba5fef82429a4dc
Diego Montivero <diego.montivero91@gmail.com>
2023-02-16 23:45:18 -0300

    Added formatTrackCount

--- src/global/utils.ts
+++ src/global/utils.ts
@@ -1,7 +1,11 @@
+export function formatDate(date: string): string {
+  return new Intl.DateTimeFormat().format(new Date(date));
+}
+
 export function formatTime(time: number): string {
   return new Date(time * 1000).toISOString().slice(15, 19);
 }
 
-export function formatDate(date: string): string {
-  return new Intl.DateTimeFormat().format(new Date(date));
+export function formatTrackCount(count: number): string {
+  return `${count} Track${count !== 1 ? "s" : ""}`;
 }
