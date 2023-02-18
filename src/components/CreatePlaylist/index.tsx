faa97553c5ca012dac72356bb64f1563859ea0d0
Diego Montivero <diego.montivero91@gmail.com>
2023-02-18 02:33:36 -0300

    Renamed component

---
+++ src/components/CreatePlaylist/index.tsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import { FontAwesomeIcon as Icon } from "@fortawesome/react-fontawesome";
+import { faPlus, faSpinner } from "@fortawesome/free-solid-svg-icons";
+
+import { useMeret } from "@context/Meret";
+import Input from "@components/Input";
+
+export default function CreatePlaylist() {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const { meret } = useMeret();
+
+  async function handleCreate(input: string) {
+    if (isLoading) return;
+
+    setIsLoading(true);
+    const response = await meret.create(input);
+    setIsLoading(false);
+  }
+
+  return (
+    <Input onSubmit={handleCreate} placeholder="Create Playlist">
+      <Icon icon={isLoading ? faSpinner : faPlus} size="lg" spin={isLoading} />
+    </Input>
+  );
+}
