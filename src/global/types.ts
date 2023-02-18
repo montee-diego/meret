269b61b5bc518e5a5079410c0e19d1950becfb25
Diego Montivero <diego.montivero91@gmail.com>
2023-02-18 02:39:33 -0300

    Updated types

--- src/global/types.ts
+++ src/global/types.ts
@@ -8,7 +8,8 @@ export interface IPlaylistMin {
   name: string;
 }
 
-export interface IPlaylist extends IDocument {
+// temp interface, needs updating
+export interface IPlaylistCard extends IDocument {
   author: {
     name: string;
     image: string;
@@ -24,15 +25,38 @@ export interface IPlaylist extends IDocument {
 }
 
 export interface ITrack extends IDocument {
-  _key?: string;
+  // _key?: string;
   artist: string;
   audio: string;
   cover: string;
   date: string;
   genres: string[];
   length: number;
-  slug: {
-    current: string;
-  };
+  // slug: {
+  //   current: string;
+  // };
   title: string;
+}
+
+export interface IPlaylistTrack extends ITrack {
+  _key: string;
+}
+
+export interface IPlaylist extends IDocument {
+  author: {
+    name: string;
+    image: string;
+  };
+  name: string;
+  total: number;
+  tracks: IPlaylistTrack[];
+  user: {
+    isAuthor: boolean;
+    isSub: boolean;
+  };
+}
+
+export interface ISelected {
+  index: number;
+  track: IPlaylistTrack | ITrack;
 }
