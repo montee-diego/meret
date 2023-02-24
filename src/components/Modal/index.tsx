80158e907105bf0b5679c749bf2e9c9d9e2a7032
Diego Montivero <diego.montivero91@gmail.com>
2023-02-24 20:21:22 -0300

    Renamed prop toggleModal to closeModal

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -2,17 +2,17 @@ import type { ReactNode } from "react";
 import { createPortal } from "react-dom";
 
 import FocusTrap from "@accessibility/FocusTrap";
-import css from "./index.module.css";
+import Style from "./index.module.css";
 
 interface IProps {
   children: ReactNode;
-  toggleOpen: () => void;
+  closeModal: () => void;
 }
 
-export default function Modal({ children, toggleOpen }: IProps) {
+export default function Modal({ children, closeModal }: IProps) {
   return createPortal(
-    <div className={css.Container}>
-      <FocusTrap active className={css.Content} cancelEvent={toggleOpen}>
+    <div className={Style.Container}>
+      <FocusTrap active className={Style.Content} cancelEvent={closeModal}>
         {children}
       </FocusTrap>
     </div>,
