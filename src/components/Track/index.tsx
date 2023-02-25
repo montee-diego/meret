a61f4e96301df307327a4b271b5b576ef69a5302
Diego Montivero <diego.montivero91@gmail.com>
2023-02-24 21:05:40 -0300

    Added fieldset to handle track lock on removal

--- src/components/Track/index.tsx
+++ src/components/Track/index.tsx
@@ -6,7 +6,7 @@ import { faPlay, faEllipsisVertical } from "@fortawesome/free-solid-svg-icons";
 import { formatTime } from "@global/utils";
 import ButtonIcon from "@components/ButtonIcon";
 import Cover from "@components/Cover";
-import css from "./index.module.css";
+import Style from "./index.module.css";
 
 interface IProps {
   data: ISelected;
@@ -18,27 +18,29 @@ export default function Track({ data, play, menu }: IProps) {
   const { track } = data;
 
   return (
-    <div className={css.Container}>
-      <Cover cover={track.cover} size="3rem" />
+    <fieldset data-key={track._key}>
+      <div className={Style.Container}>
+        <Cover cover={track.cover} size="3rem" />
 
-      <ButtonIcon onClick={() => play(data)} aria-label="play">
-        <Icon size="lg" icon={faPlay} transform="right-1 up-0.5" />
-      </ButtonIcon>
+        <ButtonIcon onClick={() => play(data)} aria-label="play">
+          <Icon size="lg" icon={faPlay} transform="right-1 up-0.5" />
+        </ButtonIcon>
 
-      <div className={css.Data}>
-        <p>{track.title}</p>
-        <p>{track.artist}</p>
+        <div className={Style.Data}>
+          <p>{track.title}</p>
+          <p>{track.artist}</p>
+        </div>
+
+        <div className={Style.Metadata}>
+          <p>{track.genres.join(", ")}</p>
+          <p>{track.date}</p>
+          <p className={Style.Length}>{formatTime(track.length)}</p>
+        </div>
+
+        <ButtonIcon onClick={(e) => menu(e, data)} aria-label="track menu">
+          <Icon size="xl" icon={faEllipsisVertical} />
+        </ButtonIcon>
       </div>
-
-      <div className={css.Metadata}>
-        <p>{track.genres.join(", ")}</p>
-        <p>{track.date}</p>
-        <p className={css.Length}>{formatTime(track.length)}</p>
-      </div>
-
-      <ButtonIcon onClick={(e) => menu(e, data)} aria-label="track menu">
-        <Icon size="xl" icon={faEllipsisVertical} />
-      </ButtonIcon>
-    </div>
+    </fieldset>
   );
 }
